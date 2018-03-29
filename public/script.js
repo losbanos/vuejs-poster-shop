eb39ef1af9d0f16f5b8f521aa29b292cfa619e0a
kimtaehoon <kimtaehoon@smilegate.com>
2018-03-29 18:30:58 +0900

    [project-1]21

--- public/script.js
+++ public/script.js
@@ -1,1 +1,20 @@
-console.log('It works.');
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            total: 0,
+            items: [
+                {title: 'abc', id: 1},
+                {title: 'ABC', id: 2},
+                {title: 'Title Item', id: 3}
+            ],
+            cart: []
+        }
+    },
+    methods: {
+        addItem(index) {
+            this.total += 9.99;
+            this.cart.push(this.items[index]);
+        }
+    }
+});
